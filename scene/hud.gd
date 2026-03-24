a8c96dd0eea530d3b02fc6575d0f386403985600
phamngoctrungnhan-jay04 <phamngoctrungnhan0901@gmail.com>
2026-03-24 23:34:34 +0700

    XuLyVaCham

--- scene/hud.gd
+++ scene/hud.gd
@@ -1,34 +1,6 @@
 extends CanvasLayer
 
-# Virtual directional state
-var _up    := false
-var _down  := false
-var _left  := false
-var _right := false
-
-@onready var btn_w := $Buttons/W
-@onready var btn_a := $Buttons/A
-@onready var btn_s := $Buttons/S
-@onready var btn_d := $Buttons/D
-
 func _ready():
-	_connect_btn(btn_w, "ui_up")
-	_connect_btn(btn_a, "ui_left")
-	_connect_btn(btn_s, "ui_down")
-	_connect_btn(btn_d, "ui_right")
-
-func _connect_btn(btn: Button, action: String):
-	btn.button_down.connect(func(): _press(action))
-	btn.button_up.connect(func(): _release(action))
-
-func _press(action: String):
-	var ev := InputEventAction.new()
-	ev.action = action
-	ev.pressed = true
-	Input.parse_input_event(ev)
-
-func _release(action: String):
-	var ev := InputEventAction.new()
-	ev.action = action
-	ev.pressed = false
-	Input.parse_input_event(ev)
+	# Giao diện HUD hiện tại không có các nút điều khiển, 
+	# tôi đã xóa code tìm nút để tránh lỗi.
+	pass
--- scene/player.gd
+++ scene/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody3D
 
-const SPEED = 22.5
-const JUMP_VELOCITY = 15.0
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * 4.5
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
