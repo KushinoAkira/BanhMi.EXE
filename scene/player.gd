153662f308ca3f702b9cfb1ed6cd71ef9cc795ab
Duy Hieu <duyhieunguyen2672004@gmail.com>
2026-03-24 22:11:13 +0700

    Fix player height, add lighting/sky, WASD support; update .gitignore

---
+++ scene/player.gd
@@ -0,0 +1,57 @@
+extends CharacterBody3D
+
+const SPEED = 22.5
+const JUMP_VELOCITY = 15.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * 4.5
+
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+@export var mouse_sensitivity := 0.002
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		head.rotate_y(-event.relative.x * mouse_sensitivity)
+		camera.rotate_x(-event.relative.y * mouse_sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+	
+	if event.is_action_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom movement actions.
+	var input_dir := Vector2(
+		float(Input.is_key_pressed(KEY_D) or Input.is_action_pressed("ui_right")) -
+		float(Input.is_key_pressed(KEY_A) or Input.is_action_pressed("ui_left")),
+		float(Input.is_key_pressed(KEY_S) or Input.is_action_pressed("ui_down")) -
+		float(Input.is_key_pressed(KEY_W) or Input.is_action_pressed("ui_up"))
+	).limit_length(1.0)
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
